34123c2e0a066c61d76e3ae8e56a56b12ff635d4
rob <rob@robs-Mac-mini-26.local>
2022-06-08 09:02:50 -0400

    op_scripts

--- brewblox_flowmeters/pio_clean.py
+++ brewblox_flowmeters/pio_clean.py
@@ -2,6 +2,5 @@ import subprocess
 
 pio_clean = subprocess.run('pio run -t clean', shell=True)
 
-
 if __name__ == '__main__':
     pio_clean
--- brewblox_flowmeters/pio_upload.py
+++ brewblox_flowmeters/pio_upload.py
@@ -1,8 +1,6 @@
 import subprocess
 
-upload_command = 'pio run -t upload'
-pio_upload = subprocess.run(upload_command, shell=True)
-
+pio_upload = subprocess.run('pio run -t upload', shell=True)
 
 if __name__ == '__main__':
     pio_upload
