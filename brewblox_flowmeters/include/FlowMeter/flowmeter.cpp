6156db044e507c10e58ffe15cfec8d8fb68eb9e3
rob <rob@robs-Mac-mini-26.local>
2022-06-08 08:16:23 -0400

    removed unused libraries

--- brewblox_flowmeters/include/FlowMeter/flowmeter.cpp
+++ brewblox_flowmeters/include/FlowMeter/flowmeter.cpp
@@ -1,19 +1,16 @@
-#include <iostream>
 #include "ArduinoJson.h"
 #include "EspMQTTClient.h"
-#include "ESP32HTTPUpdateServer.h"
 
+//#include "flowmeter.hpp"
+//#include "flow_config.hpp"
 #include "FlowMeter/flowmeter.hpp"
 #include "FlowMeter/flow_config.hpp"
 
+FlowMeter f1(_SPIN1, _FLOW1, _YFS402B);
+
 EspMQTTClient client(_SSID, _PASS, _MQTTHOST, _CLIENTID, _MQTTPORT);
 
-FlowMeter f1(_SPIN1, _FLOW1, _YFS402B);
-FlowMeter f2(_SPIN2, _FLOW2, _YFS402B);
-
-void pulseCounter1() { f1.pulse_count++; }
-void pulseCounter2() { f2.pulse_count++; }
-
+void pulseCounter() { f1.pulse_count++; }
 void onConnectionEstablished(void);
 void publishData(void);
 
@@ -21,41 +18,30 @@ void setup()
 {
     // Initialize a serial connection for reporting values to the host
     Serial.begin(115200);
-
-    client.enableHTTPWebUpdater();
-    client.setMaxPacketSize(4000);
-    client.enableOTA();
-    // client.enableDebuggingMessages();
-
+    
     // WiFi
     WiFi.disconnect(true);
     delay(1000);
     WiFi.begin(_SSID, _PASS);
-
+    
     uint8_t failed_connections = 0;
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
         Serial.println("connecting..");
         failed_connections ++;
-        if (failed_connections > 20)
+        if (failed_connections > 20) 
         {
             Serial.println("restarting..");
             ESP.restart();
         }
     }
-
+    
     Serial.print("Connected to ");
     Serial.println(WiFi.localIP());
 
     pinMode(f1.sensor_pin, INPUT_PULLUP);
-    pinMode(f2.sensor_pin, INPUT_PULLUP);
-
-    digitalWrite(f1.sensor_pin, HIGH);
-    digitalWrite(f2.sensor_pin, HIGH);
-
-    attachInterrupt(f1.sensor_pin, pulseCounter1, FALLING);
-    attachInterrupt(f2.sensor_pin, pulseCounter2, FALLING);
+    attachInterrupt(f1.sensor_pin, pulseCounter, FALLING);
 }
 
 
@@ -65,27 +51,24 @@ void publish_data()
     {
         StaticJsonDocument<200> message;
         message["key"] = _CLIENTID;
-        
+
         f1.flowmeter_run();
-        message["data"][f1.name]["Flow_rate[LPM]"] = f1.flow_rate;
+        message["data"][f1.name]["Flow rate[LPM]"] = f1.flow_rate;
         message["data"][f1.name]["Total[mLs]"] = f1.total_mLs;
-        attachInterrupt(f1.sensor_pin, pulseCounter1, FALLING);
-
-        f2.flowmeter_run();
-        message["data"][f2.name]["Flow_rate[LPM]"] = f2.flow_rate;
-        message["data"][f2.name]["Total[mLs]"] = f2.total_mLs;
-        attachInterrupt(f2.sensor_pin, pulseCounter2, FALLING);
-
+        attachInterrupt(f1.sensor_pin, pulseCounter, FALLING);
+        
         client.publish(_PUBTOPIC, message.as<String>());
         serializeJsonPretty(message, Serial);
         delay(5000);
     }
 }
 
+
 void onConnectionEstablished()
 {
-    publish_data();
+    publish_data();    
 }
+
 
 void loop()
 {
